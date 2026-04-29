d33e58357ff1d0d7c7d36cfd5e8c787c7947cd2d
Bhaskar Mamoria <pinkcityblog2000@gmail.com>
2026-04-29 12:53:10 +0530

    Production commit

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,2 +1,7 @@
-import Link from "next/link";import {posts} from "@/lib/posts";
-export default function Blog(){return <div>{posts.map(p=><Link key={p.slug} href={`/blog/${p.slug}`}>{p.title}</Link>)}</div>}
+export default function BlogPage() {
+  return (
+    <div>
+      Blog Page
+    </div>
+  );
+}
